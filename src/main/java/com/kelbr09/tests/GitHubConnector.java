9a5af90ed27c0665f4a0a822532cf84f71a65693
Brian Kelly <Brian.Kelly@ca.com>
2017-09-11 15:53:25 -0600

    counting pull requests

--- src/main/java/com/kelbr09/tests/GitHubConnector.java
+++ src/main/java/com/kelbr09/tests/GitHubConnector.java
@@ -7,7 +7,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
@@ -19,7 +23,7 @@ public class GitHubConnector {
         System.out.println(jsonString);
     }
 
-    private static JSONObject parseJson(String jsonString){
+    private JSONObject parseJson(String jsonString){
         JSONObject jsonObject = null;
         try {
             jsonObject = new JSONObject(jsonString);
@@ -30,19 +34,57 @@ public class GitHubConnector {
         return jsonObject;
     }
 
-    private static void getAllRepos(JSONArray jsonArray){
-
+    private List<JSONObject> getAllRepos(JSONArray jsonArray){
+        List<JSONObject> repoArray = new ArrayList<JSONObject>();
         for(int i = 0; i < jsonArray.length(); i++){
             try {
                 JSONObject jsonObject = (JSONObject) jsonArray.get(i);
-                System.out.println(jsonObject);
+                repoArray.add(jsonObject);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
+
+        return repoArray;
+    }
+
+    private List<JSONObject> getAllPullRequests(String repoName){
+        List<JSONObject> jsonObjects = new ArrayList<>();
+
+        String urlString = "https://api.github.com/repos/bckelly1/" + repoName + "/pulls";
+        try {
+            URL url = new URL(urlString);
+
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+            }
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+            String output;
+            JSONArray jsonArray = null;
+            while ((output = br.readLine()) != null) {
+                jsonArray = new JSONArray(output);
+            }
+
+            if(jsonArray != null){
+                for(int i = 0; i < jsonArray.length(); i++){
+                    jsonObjects.add((JSONObject) jsonArray.get(i));
+                }
+            }
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+
+        return jsonObjects;
     }
 
     public static void main(String[] args) {
+        GitHubConnector gitHubConnector = new GitHubConnector();
         try {
             URL url = new URL("https://api.github.com/users/bckelly1/repos");
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -56,15 +98,25 @@ public class GitHubConnector {
 
             String output;
             JSONArray jsonArray;
+            List<JSONObject> allRepos = new ArrayList<JSONObject>();
             System.out.println("Output from Server .... \n");
             while ((output = br.readLine()) != null) {
                 jsonArray = new JSONArray(output);
-                System.out.println("Number of repos: " + jsonArray.length());
 
-                getAllRepos(jsonArray);
+                allRepos = gitHubConnector.getAllRepos(jsonArray);
             }
 
+            System.out.println("bckelly1 Repos: " + allRepos.size());
+            for(JSONObject jsonObject : allRepos){
+                System.out.print("\t|-- " + jsonObject.get("name"));
 
+                List<JSONObject> pullRequests = gitHubConnector.getAllPullRequests(jsonObject.getString("name"));
+                System.out.println(" - " + pullRequests.size());
+                for(JSONObject pullRequest : pullRequests){
+                    System.out.println("\t\t |-- " + pullRequest.getString("name"));
+                    System.out.println("\t\t\t " + pullRequest.getString("url"));
+                }
+            }
             conn.disconnect();
 
         } catch (IOException e) {
